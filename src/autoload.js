7f83c9a312ea71ca1ef2f1c241dc38cd4b229bb6
omii <staicekramer01@gmail.com>
2023-12-20 20:56:00 +0600

    Final Project

---
+++ src/autoload.js
@@ -0,0 +1,12 @@
+//bootstrap library
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css.map";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.min.js.map";
+
+//custom 
+import "./inc/styles/style.css";
+import "./inc/scripts/script.js";
+
+//animation
+import "./inc/styles/three-dots.css";
